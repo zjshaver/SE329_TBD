1f0b721aea1986e31e6acd473b523d27da826e50
Joshua Steffensmeier <jpsteffe@gmail.com>
2016-01-27 10:19:30 -0600

    Initial face detect working

---
+++ facedetect/FaceDetect.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file objectDetection2.cpp
+ * @author A. Huaman ( based in the classic facedetect.cpp in samples/c )
+ * @brief A simplified version of facedetect.cpp, show how to load a cascade classifier and how to find objects (Face + eyes) in a video stream - Using LBP here
+ */
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+/** Function Headers */
+void detectAndDisplay( Mat frame );
+
+/** Global variables */
+String face_cascade_name = "lbpcascade_frontalface.xml";
+CascadeClassifier face_cascade;
+String window_name = "Capture - Face detection";
+/**
+ * @function main
+ */
+int main( void )
+{
+    VideoCapture capture;
+    Mat frame;
+
+    //-- 1. Load the cascade
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+
+    //-- 2. Read the video stream
+    capture.open( -1 );
+    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    while ( capture.read(frame) )
+    {
+        if( frame.empty() )
+        {
+            printf(" --(!) No captured frame -- Break!");
+            break;
+        }
+
+        //-- 3. Apply the classifier to the frame
+        detectAndDisplay( frame );
+
+        //-- bail out if escape was pressed
+        int c = waitKey(10);
+        if( (char)c == 27 ) { break; }
+    }
+    return 0;
+}
+
+/**
+ * @function detectAndDisplay
+ */
+void detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0, Size(80, 80) );
+
+    for( size_t i = 0; i < faces.size(); i++ )
+    {
+        Mat faceROI = frame_gray( faces[i] );
+        //-- Draw the face
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
+}
