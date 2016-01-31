30800e28251681e259a30f70466141f0e5ea6f10
Zach Shaver <zjshaver@iastate.edu>
2016-01-31 15:51:04 -0600

    Minimum product completegit status!

--- facemap/FaceMap.cpp
+++ facemap/FaceMap.cpp
@@ -15,10 +15,14 @@
  *
  *   See <http://www.opensource.org/licenses/bsd-license>
  */
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <sstream>
 using namespace cv;
@@ -41,6 +45,16 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
         }
     }
 }
+
+/** Function Headers */
+Mat detectAndDisplay( Mat frame );
+
+/** Global variables */
+String face_cascade_name = "../facedetect/lbpcascade_frontalface.xml";
+CascadeClassifier face_cascade;
+String window_name = "Capture - Face detection";
+
+
 int main(int argc, const char *argv[]) {
     // Check for valid command line arguments, print usage
     // if no arguments were given.
@@ -67,15 +81,16 @@ int main(int argc, const char *argv[]) {
         string error_message = "This demo needs at least 2 images to work. Please add more images to your data set!";
         CV_Error(Error::StsError, error_message);
     }
+
     // The following lines simply get the last images from
     // your dataset and remove it from the vector. This is
     // done, so that the training data (which we learn the
     // cv::LBPHFaceRecognizer on) and the test data we test
     // the model with, do not overlap.
-    Mat testSample = images[images.size() - 1];
-    int testLabel = labels[labels.size() - 1];
-    images.pop_back();
-    labels.pop_back();
+    // Mat testSample = images[images.size() - 1];
+    // int testLabel = labels[labels.size() - 1];
+    // images.pop_back();
+    // labels.pop_back();
     // The following lines create an LBPH model for
     // face recognition and train it with the images and
     // labels read from the given CSV file.
@@ -100,9 +115,54 @@ int main(int argc, const char *argv[]) {
     //
     Ptr<LBPHFaceRecognizer> model = createLBPHFaceRecognizer();
     model->train(images, labels);
+
+    // Insert FaceDetect Code and input images
+    VideoCapture capture;
+    Mat frame;
+
+    //-- 1. Load the cascade
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+
+    //-- 2. Read the video stream
+    capture.open( -1 );
+    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    while ( capture.read(frame) )
+    {
+        if( frame.empty() )
+        {
+            printf(" --(!) No captured frame -- Break!");
+            break;
+        }
+
+        //-- 3. Apply the classifier to the frame
+        Mat testSample = detectAndDisplay( frame );
+        int testLabel = 1;  // zach
+
+        // The following line predicts the label of a given
+        // test image:
+        int predictedLabel = model->predict(testSample);
+        //
+        // To get the confidence of a prediction call the model with:
+        //
+        //      int predictedLabel = -1;
+        //      double confidence = 0.0;
+        //      model->predict(testSample, predictedLabel, confidence);
+        //
+        string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, testLabel);
+        cout << result_message << endl;
+
+        //-- bail out if escape was pressed
+        int c = waitKey(10);
+        if( (char)c == 27 ) { break; break; }
+    }
+
+
+
+
     // The following line predicts the label of a given
     // test image:
-    int predictedLabel = model->predict(testSample);
+    //int predictedLabel = model->predict(testSample);
     //
     // To get the confidence of a prediction call the model with:
     //
@@ -110,18 +170,18 @@ int main(int argc, const char *argv[]) {
     //      double confidence = 0.0;
     //      model->predict(testSample, predictedLabel, confidence);
     //
-    string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, testLabel);
-    cout << result_message << endl;
+    //string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, testLabel);
+    //cout << result_message << endl;
     // First we'll use it to set the threshold of the LBPHFaceRecognizer
     // to 0.0 without retraining the model. This can be useful if
     // you are evaluating the model:
     //
-    model->setThreshold(0.0);
+    //model->setThreshold(0.0);
     // Now the threshold of this model is set to 0.0. A prediction
     // now returns -1, as it's impossible to have a distance below
     // it
-    predictedLabel = model->predict(testSample);
-    cout << "Predicted class = " << predictedLabel << endl;
+    //predictedLabel = model->predict(testSample);
+    //cout << "Predicted class = " << predictedLabel << endl;
     // Show some informations about the model, as there's no cool
     // Model data to display as in Eigenfaces/Fisherfaces.
     // Due to efficiency reasons the LBP images are not stored
@@ -139,4 +199,46 @@ int main(int argc, const char *argv[]) {
     // But should I really visualize it? Probably the length is interesting:
     cout << "Size of the histograms: " << histograms[0].total() << endl;
     return 0;
+}
+
+/**
+ * @function detectAndDisplay
+ */
+Mat detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+    Mat testCrop;
+
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0, Size(80, 80) );
+
+    for( size_t i = 0; i < faces.size(); i++ )
+    {
+        Mat faceROI = frame_gray( faces[i] );
+        //-- Draw the face
+        // Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        // ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+        int x = faces[i].x;
+        int y = faces[i].y;
+        int w = faces[i].width;
+        int h = faces[i].height;
+        rectangle( frame, Point(x,y), Point((x+w),(y+h)), Scalar( 255, 0, 0 ), 2, 8, 0 );
+        Rect ROI(x,y,w,h);
+        testCrop = frame_gray(ROI);
+
+    }
+    imshow(window_name, frame);
+    //-- Show what you got
+    if(!testCrop.empty()) {
+      //imshow( window_name, testCrop );
+      return testCrop;
+    } else {
+      //imshow( window_name, frame );
+      return frame_gray;
+    }
+
 }
