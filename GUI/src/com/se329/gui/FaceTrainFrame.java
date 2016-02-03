a560df35a89c44fc946844ef4fb8fff4871154e8
Sam Ennis <sam.c.ennis@gmail.com>
2016-02-03 10:56:45 -0600

    Made some fixes. Need to figure out why you have to click run twice to get it to start... Generates the CSV correctly, need to have it run the .exe then exit.

--- GUI/src/com/se329/gui/FaceTrainFrame.java
+++ GUI/src/com/se329/gui/FaceTrainFrame.java
@@ -49,8 +49,8 @@ import javax.swing.table.DefaultTableModel;
 
 public class FaceTrainFrame {
 
-	public static final String outputFileName = "trainedSubjects.csv";
-	
+	public static final String outputFileName = "../facemap/facesDB.csv";
+
 	public static final String exeFilePath = "../faceMap.exe";
 
 	private static FileWriter writer = null;
@@ -58,24 +58,25 @@ public class FaceTrainFrame {
 	private static JFrame frmFaceTrainer;
 
 	private static JLabel photoLabel_1;
-	
+
 	private static DefaultListModel<String> listModel = new DefaultListModel<String>();
 
-	//subjects displayed on the right side of the screen
+	// subjects displayed on the right side of the screen
 	private static ArrayList<Subject> subjects = new ArrayList<Subject>();
-	//the subject you are browsing for photos for, it is not yet in "subjects"
-	private static Subject currSub = new Subject(); 
+	// the subject you are browsing for photos for, it is not yet in "subjects"
+	private static Subject currSub = new Subject();
 
 	// Delimiter used in CSV file
 	private static final String COMMA_DELIMITER = ",";
 	private static final String NEW_LINE_SEPARATOR = "\n";
 	// File header for CSV file
 
-	private Firebase firebaseRef = new Firebase("https://torrid-heat-4382.firebaseio.com/");
+	private Firebase firebaseRef = new Firebase(
+			"https://torrid-heat-4382.firebaseio.com/");
 	private static JTable table;
 	private static JTextField nameTxt;
-	
-	private static final Object[] columnNames = {"Name", "Attendance"};
+
+	private static final Object[] columnNames = { "Name", "Attendance" };
 	private Object[][] tableData;
 
 	/**
@@ -98,57 +99,75 @@ public class FaceTrainFrame {
 	 */
 	public FaceTrainFrame() {
 		querySubjects();
-		
+
 	}
-	
-	private void querySubjects(){
+
+	private void querySubjects() {
 		System.out.println("adding listener");
-		  firebaseRef.addChildEventListener(new ChildEventListener() {
-		    
-		    @Override
-		    public void onCancelled(FirebaseError firebaseError) {
-		        System.out.println("The read failed: " + firebaseError.getMessage());
-		    }
+		firebaseRef.addChildEventListener(new ChildEventListener() {
+
 			@Override
-			public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
-				System.out.println("child added"); 
-				subjects.clear();
-				System.out.println("There are " + snapshot.getChildrenCount() + " Subjects");
-		          for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-		        	  //System.out.println(postSnapshot.getValue());
-		            Subject sub = postSnapshot.getValue(Subject.class);
-		            sub.setId(previousChildKey);
-		            System.out.println(sub.getName() + " " + sub.getTimesAttended());
-		            subjects.add(sub);
-		          }	
-		          System.out.println("Size of arraylist " + subjects.size());
-		          setTableData();
-		          initialize();
+			public void onCancelled(FirebaseError firebaseError) {
+				System.out.println("The read failed: "
+						+ firebaseError.getMessage());
 			}
+
+			@Override
+			public void onChildAdded(DataSnapshot snapshot,
+					String previousChildKey) {
+				System.out.println("child added");
+				subjects.clear();
+				System.out.println("There are " + snapshot.getChildrenCount()
+						+ " Subjects");
+				for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+					// System.out.println(postSnapshot.getValue());
+					Subject sub = postSnapshot.getValue(Subject.class);
+					sub.setId(postSnapshot.getKey());
+					System.out.println(sub.getName() + " "
+							+ sub.getTimesAttended());
+					subjects.add(sub);
+				}
+				System.out.println("Size of arraylist " + subjects.size());
+				setTableData();
+				initialize();
+			}
+
 			@Override
 			public void onChildChanged(DataSnapshot snapshot, String arg1) {
-				
+
 			}
+
 			@Override
 			public void onChildMoved(DataSnapshot arg0, String arg1) {
-				
+
 			}
+
 			@Override
 			public void onChildRemoved(DataSnapshot arg0) {
-				
+
 			}
-		  });
+		});
 	}
-	private void addSubject(){
+
+	private void addSubject() {
 		System.out.println("adding subject...");
 		Firebase subjectRef = firebaseRef.child("subjects");
 		System.out.println(currSub.getName());
-		subjectRef.push().setValue(currSub);
+		
+		//Map<String, String> subj1 = new HashMap<String, String>();
+		//subj1.put("name", currSub.getName());
+		//subj1.put("timesAttended", Integer.toString(currSub.getTimesAttended()));
+		//subj1.put( "photoPaths", currSub.getPhotoPaths().toString());
+			
+		Firebase newSubjectRef = subjectRef.push();
+		newSubjectRef.setValue(currSub);
+		//subjectRef.push().setValue(subj1);
+		currSub.setId(newSubjectRef.getKey());
 		subjects.add(currSub);
-		
+
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
-		model.addRow(new Object[]{currSub.getName(), "0"});
-		
+		model.addRow(new Object[] { currSub.getName(), "0" });
+
 		frmFaceTrainer.getContentPane().validate();
 		frmFaceTrainer.getContentPane().repaint();
 	}
@@ -170,12 +189,11 @@ public class FaceTrainFrame {
 		frmFaceTrainer.getContentPane().add(panel, BorderLayout.WEST);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[] { 202, 0, 4, 0 };
-		gbl_panel.rowHeights = new int[] { 22, 17, 25, 261, 14, 35, 23,
-				0 };
+		gbl_panel.rowHeights = new int[] { 22, 17, 25, 261, 14, 35, 23, 0 };
 		gbl_panel.columnWeights = new double[] { 1.0, 1.0, 0.0,
 				Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0,
-				0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
 
 		JLabel descLabel = new JLabel("Add a new subject to be recognized.");
@@ -186,7 +204,7 @@ public class FaceTrainFrame {
 		gbc_descLabel.gridy = 0;
 		panel.add(descLabel, gbc_descLabel);
 		descLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 11));
-		
+
 		nameTxt = new JTextField();
 		GridBagConstraints gbc_nameTxt = new GridBagConstraints();
 		gbc_nameTxt.insets = new Insets(0, 0, 5, 5);
@@ -195,108 +213,110 @@ public class FaceTrainFrame {
 		gbc_nameTxt.gridy = 1;
 		panel.add(nameTxt, gbc_nameTxt);
 		nameTxt.setColumns(10);
-		
-				JLabel nameLabel = new JLabel("Name");
-				GridBagConstraints gbc_nameLabel = new GridBagConstraints();
-				gbc_nameLabel.anchor = GridBagConstraints.WEST;
-				gbc_nameLabel.insets = new Insets(0, 0, 5, 5);
-				gbc_nameLabel.gridx = 1;
-				gbc_nameLabel.gridy = 1;
-				panel.add(nameLabel, gbc_nameLabel);
-				nameLabel.setFont(new Font("Arial Black", Font.PLAIN, 11));
-								
-										photoLabel_1 = new JLabel("Upload 10 photos...");
-										GridBagConstraints gbc_photoLabel_1 = new GridBagConstraints();
-										gbc_photoLabel_1.anchor = GridBagConstraints.WEST;
-										gbc_photoLabel_1.insets = new Insets(0, 0, 5, 5);
-										gbc_photoLabel_1.gridx = 0;
-										gbc_photoLabel_1.gridy = 2;
-										panel.add(photoLabel_1, gbc_photoLabel_1);
-										photoLabel_1
-												.setFont(new Font("Arial Rounded MT Bold", Font.ITALIC, 11));
-						
-								JButton photoButton_1 = new JButton("Select Photo");
-								GridBagConstraints gbc_photoButton_1 = new GridBagConstraints();
-								gbc_photoButton_1.anchor = GridBagConstraints.NORTHWEST;
-								gbc_photoButton_1.insets = new Insets(0, 0, 5, 5);
-								gbc_photoButton_1.gridx = 1;
-								gbc_photoButton_1.gridy = 2;
-								panel.add(photoButton_1, gbc_photoButton_1);
-								photoButton_1.setFont(new Font("Arial Black", Font.PLAIN, 11));
-								photoButton_1.setBackground(Color.CYAN);
-								photoButton_1.addActionListener(new ActionListener() {
-									public void actionPerformed(ActionEvent e) {
-										currSub.appendPhotoPath("../pics/" + selectPhoto());
-									}
-								});
-				
-						JList<String> list = new JList<String>(listModel);
-						GridBagConstraints gbc_list = new GridBagConstraints();
-						gbc_list.gridwidth = 2;
-						gbc_list.insets = new Insets(0, 0, 5, 5);
-						gbc_list.fill = GridBagConstraints.BOTH;
-						gbc_list.gridx = 0;
-						gbc_list.gridy = 3;
-						panel.add(list, gbc_list);
-						
-								JButton addButton = new JButton("Add Subject");
-								GridBagConstraints gbc_addButton = new GridBagConstraints();
-								gbc_addButton.anchor = GridBagConstraints.WEST;
-								gbc_addButton.insets = new Insets(0, 0, 5, 5);
-								gbc_addButton.gridx = 0;
-								gbc_addButton.gridy = 4;
-								panel.add(addButton, gbc_addButton);
-								addButton.setBackground(Color.GREEN);
-								addButton.addActionListener(new ActionListener() {
-									public void actionPerformed(ActionEvent e) {
-										if (nameTxt.getText().isEmpty()) {
-											JOptionPane.showMessageDialog(frmFaceTrainer, "Missing name!");
-//										} else if (currSub.getPhotoPaths().isEmpty() || currSub.getPhotoPaths().size() < 10) {
-//											JOptionPane.showMessageDialog(frmFaceTrainer,
-//													"Upload at least 10 photos!");
-										} else {
-											currSub.setName(nameTxt.getText());
-											addSubject();
-											resetValues();
-										}
-									}
-								});
-						
-								JButton cancelButton = new JButton("Cancel");
-								GridBagConstraints gbc_cancelButton = new GridBagConstraints();
-								gbc_cancelButton.anchor = GridBagConstraints.WEST;
-								gbc_cancelButton.insets = new Insets(0, 0, 0, 5);
-								gbc_cancelButton.gridx = 0;
-								gbc_cancelButton.gridy = 6;
-								panel.add(cancelButton, gbc_cancelButton);
-								cancelButton.setBackground(Color.RED);
-								cancelButton.addActionListener(new ActionListener() {
-									public void actionPerformed(ActionEvent e) {
-										resetValues();
-									}
-								});
-								
-								JButton runButton = new JButton("Run");
-								runButton.setBackground(Color.YELLOW);
-								GridBagConstraints gbc_runButton = new GridBagConstraints();
-								gbc_runButton.insets = new Insets(0, 0, 0, 5);
-								gbc_runButton.gridx = 1;
-								gbc_runButton.gridy = 6;
-								panel.add(runButton, gbc_runButton);
-								runButton.addActionListener(new ActionListener() {
-									public void actionPerformed(ActionEvent e) {
-								
-										System.out.println("Closing frame and starting executable.");
-										createFile();
-										
-										// Close frame
-//										frmFaceTrainer.setVisible(false);
-//										frmFaceTrainer.dispose();
-										
-										//startFaceRecognition();
-										
-									}
-								});
+
+		JLabel nameLabel = new JLabel("Name");
+		GridBagConstraints gbc_nameLabel = new GridBagConstraints();
+		gbc_nameLabel.anchor = GridBagConstraints.WEST;
+		gbc_nameLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_nameLabel.gridx = 1;
+		gbc_nameLabel.gridy = 1;
+		panel.add(nameLabel, gbc_nameLabel);
+		nameLabel.setFont(new Font("Arial Black", Font.PLAIN, 11));
+
+		photoLabel_1 = new JLabel("Upload 10 photos...");
+		GridBagConstraints gbc_photoLabel_1 = new GridBagConstraints();
+		gbc_photoLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_photoLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_photoLabel_1.gridx = 0;
+		gbc_photoLabel_1.gridy = 2;
+		panel.add(photoLabel_1, gbc_photoLabel_1);
+		photoLabel_1
+				.setFont(new Font("Arial Rounded MT Bold", Font.ITALIC, 11));
+
+		JButton photoButton_1 = new JButton("Select Photo");
+		GridBagConstraints gbc_photoButton_1 = new GridBagConstraints();
+		gbc_photoButton_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_photoButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_photoButton_1.gridx = 1;
+		gbc_photoButton_1.gridy = 2;
+		panel.add(photoButton_1, gbc_photoButton_1);
+		photoButton_1.setFont(new Font("Arial Black", Font.PLAIN, 11));
+		photoButton_1.setBackground(Color.CYAN);
+		photoButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currSub.appendPhotoPath("../pics/" + selectPhoto());
+			}
+		});
+
+		JList<String> list = new JList<String>(listModel);
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridwidth = 2;
+		gbc_list.insets = new Insets(0, 0, 5, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 0;
+		gbc_list.gridy = 3;
+		panel.add(list, gbc_list);
+
+		JButton addButton = new JButton("Add Subject");
+		GridBagConstraints gbc_addButton = new GridBagConstraints();
+		gbc_addButton.anchor = GridBagConstraints.WEST;
+		gbc_addButton.insets = new Insets(0, 0, 5, 5);
+		gbc_addButton.gridx = 0;
+		gbc_addButton.gridy = 4;
+		panel.add(addButton, gbc_addButton);
+		addButton.setBackground(Color.GREEN);
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (nameTxt.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(frmFaceTrainer,
+							"Missing name!");
+					// } else if (currSub.getPhotoPaths().isEmpty() ||
+					// currSub.getPhotoPaths().size() < 10) {
+					// JOptionPane.showMessageDialog(frmFaceTrainer,
+					// "Upload at least 10 photos!");
+				} else {
+					currSub.setName(nameTxt.getText());
+					addSubject();
+					resetValues();
+				}
+			}
+		});
+
+		JButton cancelButton = new JButton("Cancel");
+		GridBagConstraints gbc_cancelButton = new GridBagConstraints();
+		gbc_cancelButton.anchor = GridBagConstraints.WEST;
+		gbc_cancelButton.insets = new Insets(0, 0, 0, 5);
+		gbc_cancelButton.gridx = 0;
+		gbc_cancelButton.gridy = 6;
+		panel.add(cancelButton, gbc_cancelButton);
+		cancelButton.setBackground(Color.RED);
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resetValues();
+			}
+		});
+
+		JButton runButton = new JButton("Run");
+		runButton.setBackground(Color.YELLOW);
+		GridBagConstraints gbc_runButton = new GridBagConstraints();
+		gbc_runButton.insets = new Insets(0, 0, 0, 5);
+		gbc_runButton.gridx = 1;
+		gbc_runButton.gridy = 6;
+		panel.add(runButton, gbc_runButton);
+		runButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				System.out.println("Closing frame and starting executable.");
+				createFile();
+
+				// Close frame
+				// frmFaceTrainer.setVisible(false);
+				// frmFaceTrainer.dispose();
+
+				// startFaceRecognition();
+
+			}
+		});
 
 		JPanel panel_1 = new JPanel();
 		frmFaceTrainer.getContentPane().add(panel_1, BorderLayout.CENTER);
@@ -313,10 +333,10 @@ public class FaceTrainFrame {
 		gbc_table.gridx = 0;
 		gbc_table.gridy = 0;
 		panel_1.add(table, gbc_table);
-		
+
 		frmFaceTrainer.getContentPane().validate();
 		frmFaceTrainer.getContentPane().repaint();
-		
+
 	}
 
 	private static String selectPhoto() {
@@ -336,18 +356,19 @@ public class FaceTrainFrame {
 	}
 
 	private boolean createFile() {
+		
 		try {
-			writer = new FileWriter(outputFileName, true);
-			
+			writer = new FileWriter(outputFileName, false);
+
 			System.out.println("creating file");
-			
-			for(Subject sub : subjects){
+
+			for (Subject sub : subjects) {
 				writer.append(sub.getId());
 				writer.append(COMMA_DELIMITER);
 				writer.append(sub.getName());
 				writer.append(COMMA_DELIMITER);
 				writer.append(sub.getTimesAttended() + "");
-				for(String str : sub.getPhotoPaths()){
+				for (String str : sub.getPhotoPaths()) {
 					writer.append(COMMA_DELIMITER);
 					writer.append(str);
 				}
@@ -368,39 +389,40 @@ public class FaceTrainFrame {
 		nameTxt.setText("");
 		currSub = new Subject();
 	}
-	
-	private void startFaceRecognition(){
-		//TODO
-		
+
+	private void startFaceRecognition() {
+		// TODO
+
 		if (new File(exeFilePath).exists()) {
-		    try {
+			try {
 
-		        ProcessBuilder pb = new ProcessBuilder(exeFilePath, outputFileName);
-		        pb.redirectError();
-		        Process p = pb.start();
-		        InputStream is = p.getInputStream();
-		        int value = -1;
-		        while ((value = is.read()) != -1) {
-		            System.out.print((char) value);
-		        }
+				ProcessBuilder pb = new ProcessBuilder(exeFilePath,
+						outputFileName);
+				pb.redirectError();
+				Process p = pb.start();
+				InputStream is = p.getInputStream();
+				int value = -1;
+				while ((value = is.read()) != -1) {
+					System.out.print((char) value);
+				}
 
-		        int exitCode = p.waitFor();
+				int exitCode = p.waitFor();
 
-		        System.out.println("exited with " + exitCode);
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
+				System.out.println("exited with " + exitCode);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
-		    System.err.println(exeFilePath + " does not exist");
+			System.err.println(exeFilePath + " does not exist");
 		}
 	}
-	
-	private void setTableData(){
+
+	private void setTableData() {
 		int i = 1;
-		tableData = new Object[subjects.size()+1][2];
+		tableData = new Object[subjects.size() + 1][2];
 		tableData[0] = columnNames;
-		for(Subject sub : subjects){
-			Object[] row = {sub.getName(), sub.getTimesAttended()};
+		for (Subject sub : subjects) {
+			Object[] row = { sub.getName(), sub.getTimesAttended() };
 			tableData[i] = row;
 			i++;
 		}
