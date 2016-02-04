f46a85470cb71c16fee788f2686f5fc318cf5a65
Zach Shaver <zjshaver@iastate.edu>
2016-02-03 21:39:14 -0600

    Fixed my java mess

--- GUI/src/com/se329/gui/FaceTrainFrame.java
+++ GUI/src/com/se329/gui/FaceTrainFrame.java
@@ -435,10 +435,9 @@ public class FaceTrainFrame {
 				InputStream is = p.getInputStream();
 				int value = -1;
 				while ((value = is.read()) != -1) {
-					System.out.print((char) value);*/
-					Process process = new ProcessBuilder("./facemap/FaceMap.exe","./facesDB.csv").start();
-				}
-
+					System.out.print((char) value);
+				}*/
+				Process p = new ProcessBuilder("./facemap/FaceMap.exe","./facesDB.csv").start();
 				int exitCode = p.waitFor();
 
 				System.out.println("exited with " + exitCode);
